374358375d69997551273aa3555fb718714bc50a
yanter <terry88@gmail.com>
2026-02-15 07:33:11 +1100

    Initial Commit

---
+++ src/config/zh/siteData.json.ts
@@ -0,0 +1,25 @@
+import { type SiteDataProps } from "../types/configDataTypes";
+
+// Update this file with your site specific information
+const siteData: SiteDataProps = {
+	name: "Terrys Events",
+	// Your website's title and description (meta fields)
+	title: "Terrys Events - Event Blog",
+	description:
+		"The beautiful event blog theme",
+	// Your information!
+	author: {
+		name: "Terry",
+		email: "contact@terrys.events.com",
+		twitter: "Terrys_Events",
+		slug: "main-author", // this must match an author slug in the authors content collection at src/data/authors/{slug}
+	},
+
+	// default image for meta tags if the page doesn't have an image already
+	defaultImage: {
+		src: "/images/cosmic-themes-logo.jpg",
+		alt: "Cosmic Themes Logo",
+	},
+};
+
+export default siteData;
